46881f7437c2c9f598c66302b99088e1c64df562
Egor Danchenkov <icepnyx@gmail.com>
2016-11-05 14:40:59 +0200

    Add skeleton, normalise, use pre-compiled scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
 module.exports = {
     entry : './src/app.js',
     output : {
         path : './bundles',
         filename : 'app.bundle.js'
+    },
+    resolve : {
+        root : [
+            path.resolve('./src'),
+            path.resolve('./styles')
+        ],
+        extensions : ['', '.js', '.scss'],
+        exclude : [
+            path.resolve('./node_modules')
+        ],
+        modulesDirectories : ['node_modules']
     },
     devtool : 'source-map',
     module : {
@@ -14,8 +28,11 @@ module.exports = {
             },
             {
                 test : /\.scss$/,
-                loaders : ['style', 'css?sourceMap', 'sass?sourceMap']
+                loader : ExtractTextPlugin.extract('style', 'css!sass')
             }
         ]
-    }
+    },
+    plugins : [
+        new ExtractTextPlugin('styles.css')
+    ]
 };
