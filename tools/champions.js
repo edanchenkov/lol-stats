331d0e4bdca47b9ba111465354b8b32116ecaaf1
Egor Danchenkov <icepnyx@gmail.com>
2016-11-13 17:16:50 +0200

    Restructure champions.json, id -> champion

--- tools/champions.js
+++ tools/champions.js
@@ -3,8 +3,18 @@ import Api from './../src/api';
 
 let file = 'data/champions.json';
 
-Api.getChampions('euw').then((res) => {
-    fs.writeFile(file, JSON.stringify(res.body), function (err) {
+Api.getChampions('euw').end((err, res) => {
+
+    let champions = JSON.parse(res.text).data;
+    let content = {};
+
+    for (let key in champions) {
+        if (champions.hasOwnProperty(key)) {
+            content[champions[key].id] = champions[key];
+        }
+    }
+
+    fs.writeFile(file, JSON.stringify(content), function (err) {
         if (err) {
             return console.log(err);
         }
