9e734aae499bad1ed5114095bf280d5f235e81c1
Egor Danchenkov <icepnyx@gmail.com>
2016-11-13 18:13:56 +0200

    Create post_install script

---
+++ post_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "> Recreate data folder"
+rm -rf data
+mkdir data
+
+echo "> Getting Champions Data"
+babel-node tools/champions.js
+
+echo "> Getting Realms Data"
+babel-node tools/realms.js
