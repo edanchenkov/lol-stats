023857cdb4688db7b621127a622502e50d7c5a07
Egor Danchenkov <icepnyx@gmail.com>
2016-11-16 22:37:48 +0200

    Update post install script

--- post_install.sh
+++ post_install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-#echo "> Recreate data folder"
-#rm -rf data
-#mkdir data
-#
-#echo "> Getting Champions Data"
-#babel-node tools/champions.js
-#
-#echo "> Getting Realms Data"
-#babel-node tools/realms.js
+echo "> Go to www folder"
+cd www
+
+echo "> Recreate data folder"
+rm -rf data
+mkdir data
+
+echo "> Getting Champions Data"
+babel-node tools/champions.js
+
+echo "> Getting Realms Data"
+babel-node tools/realms.js
+
+echo "> Build client webpack"
+webpack
