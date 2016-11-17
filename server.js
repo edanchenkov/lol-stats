438321388ce344f409cec210c2fbe2756de2def6
Egor Danchenkov <icepnyx@gmail.com>
2016-11-17 19:11:01 +0200

    Update start script. Use sendFile

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var app = express();
 app.use('/bundles', express.static('www/bundles'));
 
 app.get('/', function (req, res) {
-    res.sendfile('www/index.html');
+    res.sendFile('www/index.html', { root : __dirname });
 });
 
 var port = process.env.PORT || 8080;
 
 app.listen(port, function (req, res) {
-    console.log('Running server on port');
+    console.log('Running server on port', port);
 });
