fc76817e2612f66783a9f711318f5f163d024a15
Egor Danchenkov <icepnyx@gmail.com>
2016-11-20 17:29:08 +0200

    Update configs

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ let config = {
         host : 'api.pvp.net',
         region : ''
     },
-    nodeApp : {
-
-    },
+    nodeApp : {},
     keys : {
         development : 'RGAPI-9cc57470-7927-4c11-a671-09c43ecc25b1',
         production : '< You API key >'
@@ -17,7 +15,22 @@ let config = {
         'na' : 'North America',
         'ru' : 'Russia',
         'kr' : 'Korea'
-    }
+    },
+    queueTypes : [
+        'RANKED_TEAM_5x5',
+        'RANKED_FLEX_SR'
+    ],
+    playerStatSummaryTypes : [
+        'RankedTeam5x5',
+        'RankedSolo5x5',
+        'RankedFlexSR'
+    ],
+    seasons : [
+        'PRESEASON2016',
+        'SEASON2016',
+        'PRESEASON2017',
+        'SEASON2017'
+    ]
 };
 
 export default config;
